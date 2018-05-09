e5049dfba074e81d6580f7b330bb74df64df3679
Hugo Trindade <hugotrindade.dev@gmail.com>
2018-05-09 04:28:06 -0300

    Obtendo usuario por id

--- src/main/java/com/hugotrindade/services/UserService.java
+++ src/main/java/com/hugotrindade/services/UserService.java
@@ -1,12 +1,14 @@
 package com.hugotrindade.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hugotrindade.domain.User;
 import com.hugotrindade.repository.UserRepository;
+import com.hugotrindade.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> optional = repository.findById(id);
+		return optional.orElseThrow( () -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
