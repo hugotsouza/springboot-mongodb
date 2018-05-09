dcb8b06bbb4f8597f99e495131da79c7b5cb22f8
Hugo Trindade <hugotrindade.dev@gmail.com>
2018-05-09 06:22:33 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/hugotrindade/config/Instantiation.java
+++ src/main/java/com/hugotrindade/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.hugotrindade.domain.Post;
 import com.hugotrindade.domain.User;
 import com.hugotrindade.dto.AuthorDTO;
+import com.hugotrindade.dto.CommentDTO;
 import com.hugotrindade.repository.PostRepository;
 import com.hugotrindade.repository.UserRepository;
 
@@ -39,6 +40,12 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("2018/03/21"), new AuthorDTO(alex));
+		CommentDTO c2  = new CommentDTO("Aproveite!", sdf.parse("2018/03/22"), new AuthorDTO(bob));
+		CommentDTO c3  = new CommentDTO("Tenha um ótimo dia!", sdf.parse("2018/03/23"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post1.getComments().add(c3);
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
